91675a90f75a8c007733b8d5bf95d1cfae87ecfc
wangziguang <wangziguang@skyworth.com>
2020-12-29 15:01:32 +0800

    [编译期版] 1.增加idemo-plugin模块，将用于解决子module里无法使用新建的类问题 [新增]

---
+++ idemo-plugin/src/main/groovy/cc/ives/idemo/plugin/IDemoPlugin.groovy
@@ -0,0 +1,12 @@
+package cc.ives.idemo.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class IDemoPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
