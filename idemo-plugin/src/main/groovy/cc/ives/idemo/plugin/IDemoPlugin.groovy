f8d32927cbd4ea0a33340dfa1c4cc285938d1e41
wangziguang <wangziguang@skyworth.com>
2020-12-29 19:21:11 +0800

    [编译期版] 1.更新plugin module [修改]

--- idemo-plugin/src/main/groovy/cc/ives/idemo/plugin/IDemoPlugin.groovy
+++ idemo-plugin/src/main/groovy/cc/ives/idemo/plugin/IDemoPlugin.groovy
@@ -1,5 +1,13 @@
 package cc.ives.idemo.plugin
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.FeatureExtension
+import com.android.build.gradle.FeaturePlugin
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.api.BaseVariant
+import org.gradle.api.DomainObjectSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,6 +15,56 @@ class IDemoPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        printPaths(project)
+        File mBuildDir = new File(project.getBuildDir(), "FunctionAdder")
+        project.android.registerTransform(new IDemoTransform(project))
+    }
 
+    void printPaths(Project project){
+        println "=========print paths==================${hashCode()}"
+        System.out.println("build目录:"+project.getBuildDir().absolutePath)
+        project.allprojects.each {
+            println "project name:${it.name}, path:${it.path}"
+        }
+        println "rootDir is:${project.rootDir.path}"
+        println "childProject is:${project.childProjects.toMapString()}"
+        println "subProject is:${project.subprojects.toListString()}"
+        println "--------------------------------------"
+
+
+        project.plugins.all {
+            println "plugin is:${it}"
+            switch (it) {
+                case FeaturePlugin :
+                    project.extensions[FeatureExtension].run {
+                        configureR2Generation(project, featureVariants)
+                        configureR2Generation(project, libraryVariants)
+                    }
+                    break
+                case LibraryPlugin :
+                    project.extensions[LibraryExtension].run {
+                        configureR2Generation(project, libraryVariants)
+                    }
+                    break
+                case AppPlugin :
+                    project.extensions[AppExtension].run {
+                        configureR2Generation(project, applicationVariants)
+                    }
+                    break
+            }
+        }
+    }
+
+    static void configureR2Generation(Project project, DomainObjectSet<BaseVariant> variants) {
+        variants.all { variant ->
+            def myOutputDir = project.buildDir.resolve(
+                    "generated/source/r2/${variant.dirName}")
+            println "variant is${variant.dirName},myOutputDir is:${myOutputDir}"
+
+            variant.outputs.all { output ->
+                println "output file dir:${output.dirName}"
+//                variant.registerJavaGeneratingTask(generate, outputDir)
+            }
+        }
     }
 }
