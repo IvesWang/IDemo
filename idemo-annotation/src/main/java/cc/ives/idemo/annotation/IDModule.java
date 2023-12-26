de059d63f11387902973c84c025ed405c9dabe5f
iveswang <ives@qq.com>
2023-12-26 22:56:08 +0800

    提交一些备忘和架构图

--- idemo-annotation/src/main/java/cc/ives/idemo/annotation/IDModule.java
+++ idemo-annotation/src/main/java/cc/ives/idemo/annotation/IDModule.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
  * @author wangziguang
  * @date 2020/5/23
  * @description 标记为入口activity，会放在mainactivity中
- * todo 当前是运行时方式，考虑是否可以用编译期的方式来做
  * 编译期好处：不需要遍历所有activity，可以被动过滤到添加了注解的类，后期可扩展性也更强，不一定非得activity。但编译时间会加长
  * 运行期：好坏除了更编译期反过来外，会增加运行时内存和执行时间（可忽略该时间）。
  */
